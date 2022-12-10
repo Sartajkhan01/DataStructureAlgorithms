89d734ee2f93e4d95049677fba5f06f12325cff2
Sartajkhan01 <sartajikbalkhan2000@gmail.com>
2022-12-10 23:04:15 +0530

    Merge sort program added

--- DataStructure_Algorithm/Program.cs
+++ DataStructure_Algorithm/Program.cs
@@ -11,7 +11,8 @@ namespace DataStructure_Algorithmls
             while (flag == "Y" || flag == "y")
             {
                 Console.WriteLine("Welcome To Datastructures and Algorithms");
-                Console.WriteLine("1.Permutation Using Recursion\n2.BinarySearch\n3.InsertionSort\n4.BubbleSort");
+                Console.WriteLine("1.Permutation Using Recursion\n2.BinarySearch\n3.InsertionSort\n4.BubbleSort" +
+                    "\n5.MergeSort");
                 Console.WriteLine("Enter Your Option");
                 int num = Convert.ToInt32(Console.ReadLine());
                 switch (num)
@@ -31,6 +32,10 @@ namespace DataStructure_Algorithmls
                     case 4:
                         BubbleSort bubblesort = new BubbleSort();
                         bubblesort.bubbleSort();
+                        break;
+                    case 5:
+                        MergeSort mergesort = new MergeSort();
+                        mergesort.mergeSort();
                         break;
                     default:
                         Console.WriteLine("-------Enter the valid option---");
