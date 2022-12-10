dd3921e4e7ec2205afba59540544969e3352b092
Sartajkhan01 <sartajikbalkhan2000@gmail.com>
2022-12-10 23:19:33 +0530

    searching and sorting using generics program added

--- DataStructure_Algorithm/Program.cs
+++ DataStructure_Algorithm/Program.cs
@@ -12,7 +12,7 @@ namespace DataStructure_Algorithmls
             {
                 Console.WriteLine("Welcome To Datastructures and Algorithms");
                 Console.WriteLine("1.Permutation Using Recursion\n2.BinarySearch\n3.InsertionSort\n4.BubbleSort" +
-                    "\n5.MergeSort\n6.Anagrams\n7.PrimeNumber\n8.AnagramAndPalindromes");
+                    "\n5.MergeSort\n6.Anagrams\n7.PrimeNumber\n8.AnagramAndPalindromes\n9.SearchingAndSortingUsingGenerics");
                 Console.WriteLine("Enter Your Option");
                 int num = Convert.ToInt32(Console.ReadLine());
                 switch (num)
@@ -49,9 +49,10 @@ namespace DataStructure_Algorithmls
                         AnagramAndPalindromeNumber anagramandpalindromenumber = new AnagramAndPalindromeNumber();
                         anagramandpalindromenumber.anagramAndPalindrome();
                         break;
-
-
-
+                    case 9:
+                        SearchingAndSortingUsingGenerics searchingandsortingusinggenerics = new SearchingAndSortingUsingGenerics();
+                        searchingandsortingusinggenerics.SearchingAndSorting();
+                        break;
                     default:
                         Console.WriteLine("-------Enter the valid option---");
                         break;
