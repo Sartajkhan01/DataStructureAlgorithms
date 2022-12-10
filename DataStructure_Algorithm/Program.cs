dc42fc1048b208b55257be0e6f564eb8236d1636
Sartajkhan01 <sartajikbalkhan2000@gmail.com>
2022-12-10 23:07:32 +0530

    Anagram program added

--- DataStructure_Algorithm/Program.cs
+++ DataStructure_Algorithm/Program.cs
@@ -12,7 +12,7 @@ namespace DataStructure_Algorithmls
             {
                 Console.WriteLine("Welcome To Datastructures and Algorithms");
                 Console.WriteLine("1.Permutation Using Recursion\n2.BinarySearch\n3.InsertionSort\n4.BubbleSort" +
-                    "\n5.MergeSort");
+                    "\n5.MergeSort\n6.Anagrams");
                 Console.WriteLine("Enter Your Option");
                 int num = Convert.ToInt32(Console.ReadLine());
                 switch (num)
@@ -37,6 +37,11 @@ namespace DataStructure_Algorithmls
                         MergeSort mergesort = new MergeSort();
                         mergesort.mergeSort();
                         break;
+                    case 6:
+                        Anagrams anagrams = new Anagrams();
+                        anagrams.checkAnagrams();
+                        break;
+
                     default:
                         Console.WriteLine("-------Enter the valid option---");
                         break;
